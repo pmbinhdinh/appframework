143fa710ea5a4f5507c0e9c51e3fbbbb915b9d48
Bernhard Posselt <nukeawhale@gmail.com>
2013-04-02 10:38:28 +0200

    added getUrlContent

--- core/api.php
+++ core/api.php
@@ -397,4 +397,15 @@ class API {
 	}
 
 
+	/**
+	 * Gets the content of an URL by using CURL or a fallback if it is not 
+	 * installed
+	 * @param string $url the url that should be fetched
+	 * @return string the content of the webpage
+	 */
+	public function getUrlContent($url) {
+		return \OC_Util::getUrlContent($url);
+	}
+
+
 }
