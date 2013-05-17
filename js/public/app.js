026f1c16176c154177faee294d7e8cd02dc6f8ba
Xéfir Destiny <xefir@crystalyx.net>
2013-05-17 14:33:55 +0200

    Fix https://github.com/owncloud/news/issues/181

--- js/public/app.js
+++ js/public/app.js
@@ -124,7 +124,7 @@ License along with this library.  If not, see <http://www.gnu.org/licenses/>.
   angular.module('OC').filter('ocSanitizeURL', function() {
     return function(input) {
       var regex;
-      regex = new RegExp('^(https?:\\/\\/)?' + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-a-z\\d_]*)?$', 'i', 'i');
+      regex = new RegExp('^(https?:\\/\\/)?' + '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + '((\\d{1,3}\\.){3}\\d{1,3}))' + '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + '(\\?[;&a-z\\d%_.~+=-]*)?' + '(\\#[-=a-z\\d_]*)?$', 'i', 'i');
       if (regex.test(input)) {
         return input;
       } else {
