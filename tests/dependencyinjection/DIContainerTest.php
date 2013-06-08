9ef309925a78b98ddfb2f16f3586e191c90d6dce
Morris Jobke <morris.jobke@gmail.com>
2013-06-08 11:26:32 +0200

    HttpMiddleware tests

--- tests/dependencyinjection/DIContainerTest.php
+++ tests/dependencyinjection/DIContainerTest.php
@@ -4,7 +4,9 @@
  * ownCloud - App Framework
  *
  * @author Bernhard Posselt
+ * @author Morris Jobke
  * @copyright 2012 Bernhard Posselt nukeawhale@gmail.com
+ * @copyright 2013 Morris Jobke morris.jobke@gmail.com
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
